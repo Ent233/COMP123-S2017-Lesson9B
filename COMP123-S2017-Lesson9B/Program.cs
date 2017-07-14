fb47d6eabd9636bdc7f998bf2fd33ba4982db832
Ent233 <t6478990390@gmail.com>
2017-07-13 23:00:38 -0400

    Lab24 SuperVIllain Add SuperCillain.cs Add IHasMalice.cs Change Program.cs

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*
- * Name: Tom Tsiliopoulos
- * Date: July 11, 2017
- * Description: Demo for Lesson 9
- * Version: 0.7 - Modified Driver class to implement SuperHero
+ * Name: Linlin Fan
+ * Date: July 13, 2017
+ * Description: Lab24 for Lesson 9
+ * Version: 0.8 - Modified Driver class to implement SuperHero
  */
 
 namespace COMP123_S2017_Lesson9B
@@ -21,6 +21,11 @@ namespace COMP123_S2017_Lesson9B
             superHero.AddPower("Spider Climbing", 50);
             superHero.AddPower("Webcasting", 40);
             Console.WriteLine(superHero.ToString());
+
+            SuperVillain superVillain = new SuperVillain("Super Dude LV2", 20);
+            superVillain.AddPower("Spider Climbing LV2", 50);
+            superVillain.AddPower("Webcasting LV2", 40);
+            Console.WriteLine(superVillain.ToString());
 
 
         }
---
+++ COMP123-S2017-Lesson9B/SuperVillain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2017_Lesson9B
+{
+    public class SuperVillain : SuperHuman, IHasMalice
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private int _malic;
+
+        // PUBLIC PROPERTIES
+        public int Malice
+        {
+            get
+            {
+                return this._malic;
+            }
+
+            set
+            {
+                this._malic = value;
+            }
+        }
+
+        // CONSTRUCTORS
+
+        /// <summary>
+        /// This is the main constructor for the SuperVillain class
+        /// It takes two arguments - name (string) - malice (int)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="malice"></param>
+        public SuperVillain(string name, int malice)
+            :base(name)
+        {
+            this.Malice = malice;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+    }
+}
